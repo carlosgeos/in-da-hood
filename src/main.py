0c6e9ec9ff7cfeaefbbe28b9b9e63a9bbd026213
Carlos Requena López <crequena@ulb.ac.be>
2018-12-05 02:07:18 +0100

    integration of networkx, similarity measure and plotly

--- src/main.py
+++ src/main.py
@@ -1,11 +1,77 @@
+from math import sqrt
+import numpy as np
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 
 import networkx as nx
 
+from plot import Plotter
+
+np.set_printoptions(precision=3)
+
 def main():
-    print("hello")
+    print("Hello")
+
+def mu(row, n):
+    """Computes the means for every row. This function is passed as an
+    argument to map.
+
+    """
+    return len(row[1]) / (n - 1)
+
+def sigma(adj, means):
+    """Computes the variance for a whole 2D array
+
+    """
+    variances = []
+    for i, row in enumerate(adj):
+        res = 0
+        for j, _ in row[1].items():
+            res += (1 - means[i])**2
+        variances.append(sqrt(res))
+    return variances
+
+def p_correlation(adj, means, variances):
+    """Returns a 2D matrix with the pearson correlation coefficient for
+    each element in arr
+
+    """
+    simil = np.ones((len(adj), len(adj)))
+    for i in range(len(adj)):
+        for j in range(len(adj)):
+            if i == j:
+                simil[i][j] = 1
+            else:
+                acc = 0
+                adj_node_i = adj[i][1]
+                adj_node_j = adj[j][1]
+                for k in range(len(adj)):
+                    neighbour_i = 1 if k in adj_node_i else 0
+                    neighbour_j = 1 if k in adj_node_j else 0
+                    acc += (neighbour_i - means[i]) * (neighbour_j - means[j])
+                try:
+                    simil[i, j] = ((1/(len(adj) - 3)) * acc) / (variances[i] * variances[j])
+                except ZeroDivisionError:
+                    simil[i, j] = 0
+    return simil
+
+n = 20
+G = nx.random_geometric_graph(n, 0.5)
+adj = list(G.adjacency())
+
+means = list(map(lambda x: mu(x, n), adj))
+variances = sigma(adj, means)
+similarities = p_correlation(adj, means, variances)
+
+print("Means:", means)
+print("Variances:", variances)
+print("Similarities:\n", similarities)
+
+
+plotter = Plotter(G)
+
+plotter.plot()
 
 if __name__ == '__main__':
     main()
--- src/plot.py
+++ src/plot.py
@@ -4,80 +4,88 @@ import plotly.graph_objs as go
 
 import networkx as nx
 
-G = nx.random_geometric_graph(200, 0.125)
-pos = nx.get_node_attributes(G, 'pos')
+class Plotter:
+    def __init__(self, G):
+        ""
+        self.G = G
 
-dmin = 1
-ncenter = 0
-for n in pos:
-    x, y = pos[n]
-    d=(x - 0.5) ** 2 + (y - 0.5) ** 2
-    if d < dmin:
-        ncenter = n
-        dmin = d
+        self.pos = nx.get_node_attributes(self.G, 'pos')
 
-p = nx.single_source_shortest_path_length(G, ncenter)
+        self.dmin = 1
+        self.ncenter = 0
 
-edge_trace = go.Scatter(
-    x = [],
-    y = [],
-    line = dict(width = 0.5,color = '#888'),
-    hoverinfo='none',
-    mode='lines')
+    def plot(self):
 
-for edge in G.edges():
-    x0, y0 = G.node[edge[0]]['pos']
-    x1, y1 = G.node[edge[1]]['pos']
-    edge_trace['x'] += tuple([x0, x1, None])
-    edge_trace['y'] += tuple([y0, y1, None])
+        for n in self.pos:
+            x, y = self.pos[n]
+            d=(x - 0.5) ** 2 + (y - 0.5) ** 2
+            if d < self.dmin:
+                self.ncenter = n
+                self.dmin = d
 
-node_trace = go.Scatter(
-    x = [],
-    y = [],
-    text = [],
-    mode = 'markers',
-    hoverinfo = 'text',
-    marker = dict(
-        showscale=True,
-        # colorscale options
-        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-        colorscale='YlGnBu',
-        reversescale=True,
-        color=[],
-        size=10,
-        colorbar = dict(
-            thickness = 15,
-            title = 'Node Connections',
-            xanchor = 'left',
-            titleside = 'right'
-        ),
-        line = dict(width=2)))
+        p = nx.single_source_shortest_path_length(self.G, self.ncenter)
 
-for node in G.nodes():
-    x, y = G.node[node]['pos']
-    node_trace['x'] += tuple([x])
-    node_trace['y'] += tuple([y])
+        edge_trace = go.Scatter(
+            x = [],
+            y = [],
+            line = dict(width = 0.5,color = '#888'),
+            hoverinfo='none',
+            mode='lines')
 
-for node, adjacencies in enumerate(G.adjacency()):
-    node_trace['marker']['color']+=tuple([len(adjacencies[1])])
-    node_info = '# of connections: '+str(len(adjacencies[1]))
-    node_trace['text']+=tuple([node_info])
+        for edge in self.G.edges():
+            x0, y0 = self.G.node[edge[0]]['pos']
+            x1, y1 = self.G.node[edge[1]]['pos']
+            edge_trace['x'] += tuple([x0, x1, None])
+            edge_trace['y'] += tuple([y0, y1, None])
 
-fig = go.Figure(data=[edge_trace, node_trace],
-                layout=go.Layout(
-                    title='<br>Network graph made with Python',
-                    titlefont=dict(size=16),
-                    showlegend=False,
-                    hovermode='closest',
-                    margin=dict(b=20,l=5,r=5,t=40),
-                    annotations=[ dict(
-                        text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
-                        showarrow=False,
-                        xref="paper", yref="paper",
-                        x=0.005, y=-0.002 ) ],
-                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+        node_trace = go.Scatter(
+            x = [],
+            y = [],
+            text = [],
+            mode = 'markers',
+            hoverinfo = 'text',
+            marker = dict(
+                showscale=True,
+                # colorscale options
+                #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+                #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+                #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+                colorscale='YlGnBu',
+                reversescale=True,
+                color=[],
+                size=10,
+                colorbar = dict(
+                    thickness = 15,
+                    title = 'Node Connections',
+                    xanchor = 'left',
+                    titleside = 'right'
+                ),
+            line = dict(width=2)))
 
-plotly.offline.plot(fig, filename='networkx')
+        for node in self.G.nodes():
+            x, y = self.G.node[node]['pos']
+            node_trace['x'] += tuple([x])
+            node_trace['y'] += tuple([y])
+
+        for node, adjacencies in enumerate(self.G.adjacency()):
+            node_trace['marker']['color'] += tuple([len(adjacencies[1])])
+            # node_info = '# of connections: ' + str(len(adjacencies[1]))
+            node_info = 'Node: ' + str(node)
+            node_trace['text'] += tuple([node_info])
+
+        fig = go.Figure(data=[edge_trace, node_trace],
+                        layout=go.Layout(
+                            title='<br>Community detection',
+                            titlefont=dict(size=16),
+                            showlegend=False,
+                            hovermode='closest',
+                            margin=dict(b=20,l=5,r=5,t=40),
+                        annotations=[ dict(
+                            text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
+                            showarrow=False,
+                            xref="paper", yref="paper",
+                            x=0.005, y=-0.002 ) ],
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+
+        plotly.offline.plot(fig, filename='networkx.html')
