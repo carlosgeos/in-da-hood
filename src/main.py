2af90af8b234833f6e24b846bc2c72749abe2ab0
Carlos Requena López <crequena@ulb.ac.be>
2018-12-10 23:33:28 +0100

    add search max in sim matrix

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 from math import sqrt
 import numpy as np
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
 
 import networkx as nx
 
@@ -10,8 +7,6 @@ from plot import Plotter
 
 np.set_printoptions(precision=3)
 
-def main():
-    print("Hello")
 
 def mu(row, n):
     """Computes the means for every row. This function is passed as an
@@ -19,6 +14,7 @@ def mu(row, n):
 
     """
     return len(row[1]) / (n - 1)
+
 
 def sigma(adj, means):
     """Computes the variance for a whole 2D array
@@ -32,6 +28,7 @@ def sigma(adj, means):
         variances.append(sqrt(res))
     return variances
 
+
 def p_correlation(adj, means, variances):
     """Returns a 2D matrix with the pearson correlation coefficient for
     each element in arr
@@ -41,7 +38,7 @@ def p_correlation(adj, means, variances):
     for i in range(len(adj)):
         for j in range(len(adj)):
             if i == j:
-                simil[i][j] = 1
+                simil[i][j] = np.nan
             else:
                 acc = 0
                 adj_node_i = adj[i][1]
@@ -56,8 +53,34 @@ def p_correlation(adj, means, variances):
                     simil[i, j] = 0
     return simil
 
-n = 20
-G = nx.random_geometric_graph(n, 0.5)
+
+def slink(sim):
+    tree = [{}]
+    for x in range(len(sim) - 1):
+        max_cell = np.nanmax(sim)
+        print("max_value is:", max_cell)
+        max_ij = np.unravel_index(np.nanargmax(sim), (len(sim), len(sim)))
+        print("maxij is: ", max_ij)
+        keep = min(max_ij)
+        throw = max(max_ij)
+        for i in range(len(sim)):
+            if keep != i and throw != i:
+                sim[keep][i] = max(sim[keep][i], sim[throw][i])
+                sim[i][keep] = max(sim[i][keep], sim[i][throw])
+        sim = np.delete(sim, throw, 0)
+        sim = np.delete(sim, throw, 1)
+        before = tree[x]
+        tree.append(dict(before))
+        if keep:
+            pass
+
+
+
+
+
+
+n = 100
+G = nx.random_geometric_graph(n, 0.3)
 adj = list(G.adjacency())
 
 means = list(map(lambda x: mu(x, n), adj))
@@ -68,10 +91,11 @@ print("Means:", means)
 print("Variances:", variances)
 print("Similarities:\n", similarities)
 
-
 plotter = Plotter(G)
 
 plotter.plot()
 
+slink(similarities)
+
 if __name__ == '__main__':
-    main()
+    pass
